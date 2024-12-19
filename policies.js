9d0b659617ce5c7ae4298225d7a3044af5df4091
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2024-12-19 02:53:30 +0100

    feat: add policy `restrictRepoCreationGitHub`

--- policies.js
+++ policies.js
@@ -1,7 +1,10 @@
+const github = require('./github')
+
 const policies = [{
-  name: 'enforceMFAGithub',
-  description: 'Enforce MFA on Github',
-  policy: () => {}
+  name: 'restrictRepoCreationGitHub',
+  description: 'This policy is designed to prevent members of a GitHub organization from creating new repositories. This includes public and private repositories.',
+  technicalDetails: 'This policy will set the following values for the organization(`members_allowed_repository_creation_type=none`, `members_can_create_public_repositories=false`, `members_can_create_private_repositories=false`) at the organization level.',
+  policy: github.restrictRepoCreationGitHub
 }]
 
 module.exports = policies
