c61f67691359e25cc298d1d68f64a822f7453d9e
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2024-12-19 01:19:10 +0100

    chore: linting

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const pkg = require('./package.json')
 const command = new Command()
 
 command
-.command('version')
-.description('fortSphere version')
-.action(() => {
-  console.log(`fortSphere version: ${pkg.version}`)
-  console.warn('IMPORTANT:This is an experimental version of fortSphere')
-})
+  .command('version')
+  .description('fortSphere version')
+  .action(() => {
+    console.log(`fortSphere version: ${pkg.version}`)
+    console.warn('IMPORTANT: This is an experimental version of fortSphere')
+  })
 
 command.parse(process.argv)
-
