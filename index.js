332df432e229e6f8b1fba7103aa0777d4c64d295
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2024-12-19 01:18:43 +0100

    feat: add `version` command to CLI

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Command } = require('commander')
+const pkg = require('./package.json')
+
+const command = new Command()
+
+command
+.command('version')
+.description('fortSphere version')
+.action(() => {
+  console.log(`fortSphere version: ${pkg.version}`)
+  console.warn('IMPORTANT:This is an experimental version of fortSphere')
+})
+
+command.parse(process.argv)
+
